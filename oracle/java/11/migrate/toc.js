f153ff3e17d342e82a17ba7a8580e8ae4cfd2198
xy2401 <xy2401@163.com>
2019-06-09 14:46:37 +0800

    add

---
+++ oracle/java/11/migrate/toc.js
@@ -0,0 +1,276 @@
+define(
+{
+    "toc":[
+        {
+            "heading":"Table of Contents",
+            "topics":[
+                {
+                    "title":"Getting Started",
+                    "href":"index.html#JSMIG-GUID-C25E2B1D-6C24-4403-8540-CFEA875B994A"
+                },
+                {
+                    "title":"Significant Changes in JDK 11 Release",
+                    "href":"index.html#JSMIG-GUID-561005C1-12BB-455C-AD41-00455CAD23A6",
+                    "topics":[
+                        {
+                            "title":"Removal of the Deployment Stack",
+                            "href":"index.html#JSMIG-GUID-4B3D2D73-359C-4ADA-937E-BAEA79CFDF0F"
+                        },
+                        {
+                            "title":"Removal of Java EE and CORBA Modules",
+                            "href":"index.html#JSMIG-GUID-F640FA9D-FB66-4D85-AD2B-D931174C09A3"
+                        },
+                        {
+                            "title":"Security Updates",
+                            "href":"index.html#JSMIG-GUID-8C36237D-76BB-4ADD-B026-4EB3EAA6BE99"
+                        },
+                        {
+                            "title":"Removed APIs, Tools, and Components",
+                            "href":"index.html#JSMIG-GUID-FAD4E80D-64BA-42AC-A682-38D06EE61AC6",
+                            "topics":[
+                                {
+                                    "title":"Removed APIs in JDK 11",
+                                    "href":"index.html#JSMIG-GUID-4B613B7E-B150-4D0A-835C-2393C60BE1F8"
+                                },
+                                {
+                                    "title":"Tools and Components Not Shipped with JDK 11",
+                                    "href":"index.html#JSMIG-GUID-D7936F0D-08A9-411E-AD2F-E14A38DA56A7"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "title":"Preparing For Migration",
+                    "href":"index.html#JSMIG-GUID-5657F44A-B2D7-4FB6-AAD7-295AC4533ABC",
+                    "topics":[
+                        {
+                            "title":"Download the Latest JDK",
+                            "href":"index.html#JSMIG-GUID-6FB24439-342C-496E-9D99-5F752528C7B1"
+                        },
+                        {
+                            "title":"Run Your Program Before Recompiling",
+                            "href":"index.html#JSMIG-GUID-2A6D935A-2FFB-4D3A-B982-3266A4588B49"
+                        },
+                        {
+                            "title":"Update Third-Party Libraries",
+                            "href":"index.html#JSMIG-GUID-AFD3BDEC-99FC-4F3C-946F-A1CD2D05B74B"
+                        },
+                        {
+                            "title":"Compile Your Application if Needed",
+                            "href":"index.html#JSMIG-GUID-77874D97-46F3-4DB5-85E4-2ACB5F8D760B"
+                        },
+                        {
+                            "title":"Run jdeps on Your Code",
+                            "href":"index.html#JSMIG-GUID-BA521187-60FD-4CA5-B998-58A1D05587BC"
+                        }
+                    ]
+                },
+                {
+                    "title":"Migrating From JDK 8 to Later JDK Releases",
+                    "href":"index.html#JSMIG-GUID-7744EF96-5899-4FB2-B34E-86D49B2E89B6",
+                    "topics":[
+                        {
+                            "title":"Understanding Runtime Access Warnings",
+                            "href":"index.html#JSMIG-GUID-7BB28E4D-99B3-4078-BDC4-FC24180CE82B",
+                            "topics":[
+                                {
+                                    "title":"--add-exports",
+                                    "href":"index.html#JSMIG-GUID-2F61F3A9-0979-46A4-8B49-325BA0EE8B66"
+                                },
+                                {
+                                    "title":"--add-opens",
+                                    "href":"index.html#JSMIG-GUID-12F945EB-71D6-46AF-8C3D-D354FD0B1781"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"New Version-String Scheme",
+                            "href":"index.html#JSMIG-GUID-3A71ECEF-5FC5-46FE-9BA9-88CBFCE828CB"
+                        },
+                        {
+                            "title":"Changes to the Installed JDK/JRE Image",
+                            "href":"index.html#JSMIG-GUID-D867DCCC-CEB5-4AFA-9D11-9C62B7A3FAB1",
+                            "topics":[
+                                {
+                                    "title":"Changed JDK and JRE Layout",
+                                    "href":"index.html#JSMIG-GUID-BC5E3EDE-C1FB-4B60-AF7D-8D4D7FE79C84"
+                                },
+                                {
+                                    "title":"New Class Loader Implementations",
+                                    "href":"index.html#JSMIG-GUID-A868D0B9-026F-4D46-B979-901834343F9E"
+                                },
+                                {
+                                    "title":"Removed rt.jar and tools.jar in JDK 9",
+                                    "href":"index.html#JSMIG-GUID-A78CC891-701D-4549-AA4E-B8DD90228B4B"
+                                },
+                                {
+                                    "title":"Removed Extension Mechanism in JDK 9",
+                                    "href":"index.html#JSMIG-GUID-2C896CA8-927C-4381-A737-B1D81D964B7B"
+                                },
+                                {
+                                    "title":"Removed Endorsed Standards Override Mechanism",
+                                    "href":"index.html#JSMIG-GUID-8E83E51A-88A3-4E9A-8E2A-66E1D66A966C"
+                                },
+                                {
+                                    "title":"Windows Registry Key Changes",
+                                    "href":"index.html#JSMIG-GUID-EEED398E-AE37-4D12-AB10-49F82F720027"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Removed or Changed APIs",
+                            "href":"index.html#JSMIG-GUID-F7696E02-A1FB-4D5A-B1F2-89E7007D4096",
+                            "topics":[
+                                {
+                                    "title":"Removed APIs in JDK 9 and JDK 10",
+                                    "href":"index.html#JSMIG-GUID-2A8D44E2-DB96-4C2D-9BE4-CFF9FC524397",
+                                    "topics":[
+                                        {
+                                            "title":"Removed java.* APIs",
+                                            "href":"index.html#JSMIG-GUID-44E8E37A-3000-47C6-B851-CDEB3FB50495"
+                                        },
+                                        {
+                                            "title":"Removal and Future Removal of sun.misc and sun.reflect APIs",
+                                            "href":"index.html#JSMIG-GUID-B96BD00F-12A4-493A-9907-2FFE8DA6748C"
+                                        },
+                                        {
+                                            "title":"java.awt.peer Not Accessible",
+                                            "href":"index.html#JSMIG-GUID-0C350BAB-F2C8-409E-AD3E-63831C684A55"
+                                        },
+                                        {
+                                            "title":"Removed com.sun.image.codec.jpeg Package",
+                                            "href":"index.html#JSMIG-GUID-6CCC8E89-0D1E-481C-ADF8-B6F4FB5E515C"
+                                        }
+                                    ]
+                                },
+                                {
+                                    "title":"Removed Tools Support for Compact Profiles",
+                                    "href":"index.html#JSMIG-GUID-34AA66E0-EC34-4155-A4AC-03AD3E6BC1FA"
+                                },
+                                {
+                                    "title":"Use CLDR Locale Data by Default",
+                                    "href":"index.html#JSMIG-GUID-A20F2989-BFA9-482D-8618-6CBB4BAAE310"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Deployment",
+                            "href":"index.html#JSMIG-GUID-954372A5-5954-4075-A1AF-6A9168371246",
+                            "topics":[
+                                {
+                                    "title":"Removed Launch-Time JRE Version Selection",
+                                    "href":"index.html#JSMIG-GUID-07F1C9A0-FD3A-4B31-B6F6-B049155E7432"
+                                },
+                                {
+                                    "title":"Removed Support for Serialized Applets",
+                                    "href":"index.html#JSMIG-GUID-2DEB42E0-0FB5-43A2-9EE7-27E164773093"
+                                },
+                                {
+                                    "title":"JNLP Specification Update",
+                                    "href":"index.html#JSMIG-GUID-030E50AF-5C80-4A3A-8985-211DCA4409AB"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Security Updates in JDK 9",
+                            "href":"index.html#JSMIG-GUID-D6EE05FB-6791-43B3-A610-3F4416DEE508",
+                            "topics":[
+                                {
+                                    "title":"JCE Jurisdiction Policy File Default is Unlimited",
+                                    "href":"index.html#JSMIG-GUID-BBA33F8F-17BC-493F-B39E-6A0411CFD916"
+                                },
+                                {
+                                    "title":"Create PKCS12 Keystores",
+                                    "href":"index.html#JSMIG-GUID-4B3ECBD0-3926-4CB7-8BF4-1292BB1FF6F2"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Changes to Garbage Collection",
+                            "href":"index.html#JSMIG-GUID-1F270BDA-50B0-49C8-807E-0B727CCC5169",
+                            "topics":[
+                                {
+                                    "title":"Make G1 the Default Garbage Collector",
+                                    "href":"index.html#JSMIG-GUID-9E847B7E-1F6B-4AD4-A5EE-66F8EF8BA9F6"
+                                },
+                                {
+                                    "title":"Removed GC Options",
+                                    "href":"index.html#JSMIG-GUID-977E1ED7-02BE-4A13-9B04-A719728E84BF"
+                                },
+                                {
+                                    "title":"Changes to GC Log Output",
+                                    "href":"index.html#JSMIG-GUID-055EA9F4-835E-463F-B9E1-9081B3D9E55D"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Removed Tools and Components",
+                            "href":"index.html#JSMIG-GUID-12237744-E23B-42F8-8892-26BA1BDD63F2",
+                            "topics":[
+                                {
+                                    "title":"Removed Native-Header Generation Tool (javah)",
+                                    "href":"index.html#JSMIG-GUID-0A141A7B-62A6-478C-8B8B-91FF3472B7EC"
+                                },
+                                {
+                                    "title":"Removed JavaDB",
+                                    "href":"index.html#JSMIG-GUID-B86166DA-6307-4E2F-B410-863EE5D2047F"
+                                },
+                                {
+                                    "title":"Removed the JVM TI hprof Agent",
+                                    "href":"index.html#JSMIG-GUID-BCE8E17A-7227-4E03-8678-262DAEECD5C7"
+                                },
+                                {
+                                    "title":"Removed the jhat Tool",
+                                    "href":"index.html#JSMIG-GUID-B49A964D-A2EF-4DAF-8A71-A64EF3E77C00"
+                                },
+                                {
+                                    "title":"Removed java-rmi.exe and java-rmi.cgi Launchers",
+                                    "href":"index.html#JSMIG-GUID-6840021F-C478-4BF4-902A-D466D5474733"
+                                },
+                                {
+                                    "title":"Removed Support for the IIOP Transport from the JMX RMIConnector",
+                                    "href":"index.html#JSMIG-GUID-4DD638C3-E565-42B6-AA32-00AFC16930AB"
+                                },
+                                {
+                                    "title":"Dropped Windows 32–bit Client VM",
+                                    "href":"index.html#JSMIG-GUID-59701F80-5BB1-416D-835D-C39A9112FC1E"
+                                },
+                                {
+                                    "title":"Removed Java VisualVM",
+                                    "href":"index.html#JSMIG-GUID-A3F99AB7-7425-4420-8EDE-E4237BC5FD7D"
+                                },
+                                {
+                                    "title":"Removed native2ascii Tool",
+                                    "href":"index.html#JSMIG-GUID-C86F7403-7D7C-456F-8873-18C7F1BEDE2F"
+                                }
+                            ]
+                        },
+                        {
+                            "title":"Removed macOS-Specific Features",
+                            "href":"index.html#JSMIG-GUID-97C1D0BB-D5D3-4CAD-B17D-03A87A0AAF3B",
+                            "topics":[
+                                {
+                                    "title":"Platform-Specific Desktop Features",
+                                    "href":"index.html#JSMIG-GUID-CACA0F19-2AFA-4214-BBEA-DC4D07702B2D"
+                                },
+                                {
+                                    "title":"Removed AppleScript Engine",
+                                    "href":"index.html#JSMIG-GUID-75EAB2F0-2117-421F-A3F6-4C43C564F625"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    "title":"Next Steps",
+                    "href":"index.html#JSMIG-GUID-8074FBBD-92DF-4C46-837D-86D1D55DEC84"
+                },
+                {
+                    "title":"Documentation Accessibility",
+                    "href":"index.html#JSGCT103"
+                }
+            ]
+        }
+    ]
+});
