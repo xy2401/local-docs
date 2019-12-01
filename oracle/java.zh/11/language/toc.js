8d4896d2a14d14af9f327e0db6aef794d531afce
xy2401 <xy2401@163.com>
2019-12-01 17:39:05 +0800

    cp

---
+++ oracle/java.zh/11/language/toc.js
@@ -0,0 +1,36 @@
+define(
+{
+    "toc":[
+        {
+            "heading":"Table of Contents",
+            "topics":[
+                {
+                    "title":"Java Language Changes for Java SE 11",
+                    "href":"index.html#JSLAN-GUID-8693D1DF-3DE3-4588-ABA8-397C65B4169A"
+                },
+                {
+                    "title":"Java Language Changes for Java SE 10",
+                    "href":"index.html#JSLAN-GUID-7D5FDD65-ACE4-4B3C-80F4-CC01CBD211A4"
+                },
+                {
+                    "title":"Java Language Changes for Java SE 9",
+                    "href":"index.html#JSLAN-GUID-B06D7006-D9F4-42F8-AD21-BF861747EDCF",
+                    "topics":[
+                        {
+                            "title":"More Concise try-with-resources Statements",
+                            "href":"index.html#JSLAN-GUID-A920DB06-0FD1-4F9C-8A9A-15FC979D5DA3"
+                        },
+                        {
+                            "title":"Small Language Changes in Java SE 9",
+                            "href":"index.html#JSLAN-GUID-16A5183A-DC0D-4A96-B9D8-AAC9671222DD"
+                        }
+                    ]
+                },
+                {
+                    "title":"Documentation Accessibility",
+                    "href":"index.html#JSLAN-GUID-E409CC44-9A8F-4043-82C8-6B95CD939296"
+                }
+            ]
+        }
+    ]
+});
